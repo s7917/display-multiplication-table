6328c09baab4677ff84954030b5c9a91582e550e
shubham <124447665+s7917@users.noreply.github.com>
2023-04-23 11:18:01 +0530

    Create display multiplication table.cpp

---
+++ display multiplication table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1 , n;
+    cout<<"enter the value of  n"<<endl;
+    cin>>n;
+
+    for(int i =1 ; i<=10 ; i++){
+        cout<<n<<"*"<<i<<" = "<<i*n<<endl;
+    }
+
+
+    return 0;
+}
